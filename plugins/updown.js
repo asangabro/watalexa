11a9c4989e9a70edaf508a3edd430e697e088ade
asangabro <84509909+asangabro@users.noreply.github.com>
2021-05-20 18:50:29 +0530

    Delete plugins directory

--- plugins/updown.js
+++
@@ -1,9 +0,0 @@
-/*
-# Exclusively from SL-Android project 
-# Do not use this fore any commercial thing
-# If you abuse thais bot we wil kick you from bot 
-# Do not edit (Respect to the Devaoloper) 
-# All rights reserved ©Asanga_Udara
-*/
-
-// to-do download
